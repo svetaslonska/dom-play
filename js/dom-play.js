b4cdc68c0ab288a84e52af93bbbeb8984498a646
Svitlana Slonska <svetaslonska@gmail.com>
2023-06-28 09:49:19 -0700

    update

---
+++ js/dom-play.js
@@ -0,0 +1,15 @@
+//alert("Hi from DOM Play");
+
+/*
+
+    With a single click, highlight (bold or otherwise) all lines for a specific actor in a play
+
+    Un-highlight (normal or otherwise) all lines from all other actors in the play
+    
+    Must do the work without page re-loads (i.e. it must be done via JavaScript and the DOM)
+    
+    We can change the HTML as needs
+
+*/
+
+//document.querySelector(".hamlet").style.backgroundColor = "yellow";
