bb5f1bbac8cbf84352950042ea38ec644b25ccb6
Svitlana Slonska <svetaslonska@gmail.com>
2023-06-29 09:49:57 -0700

    update

--- js/dom-play.js
+++ js/dom-play.js
@@ -13,3 +13,25 @@
 */
 
 //document.querySelector(".hamlet").style.backgroundColor = "yellow";
+
+function highlight(el){
+
+    if(el.style.backgroundColor == 'white'){//change to blue
+        el.style.backgroundColor='blue';
+    }else{//change to white
+        el.style.backgroundColor='white';
+    }
+}
+
+function myAlert(){
+    alert("I am clicked!");
+}
+
+const spans = document.querySelectorAll("#play span");
+
+console.log(spans);
+
+for(const mySpan of spans){
+    mySpan.addEventListener("click", myAlert);
+
+}
